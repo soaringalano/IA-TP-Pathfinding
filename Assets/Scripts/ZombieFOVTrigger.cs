ee1537e2c98c353493e1fc4ee32b4b011c70993b
Remiam <rallardm@gmail.com>
2024-02-20 20:59:31 -0500

    Correcting colliders and zombie roaming

--- Assets/Scripts/ZombieFOVTrigger.cs
+++ Assets/Scripts/ZombieFOVTrigger.cs
@@ -1,8 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class triggertest : MonoBehaviour
+public class ZombieFOVTrigger : MonoBehaviour
 {
     void OnTriggerStay(Collider other)
     {
